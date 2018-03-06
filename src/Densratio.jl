813e6ad7bf7579e977ffad7eb571e8c5d377d2e7
Noriaki Oshita <ponkichi0213@gmail.com>
2018-03-07 02:10:55 +0900

    [add]Densratio.jl

--- src/Densratio.jl
+++ src/Densratio.jl
@@ -1,5 +1,21 @@
 module Densratio
 
-# package code goes here
+export 
+    densratio,
+    compute_density_ratio,
+    search_sigma_and_lambda,
+    compute_kernel_Gaussian,
+    kernel_Gaussian
+
+using PyCall
+const densratio_py = PyNULL() 
+
+function __init__()
+    copy!(densratio_py, pyimport_conda("densratio", "densratio"))
+end
+
+function densratio(x, y)
+    densratio.densratio(x, y)
+end
 
 end # module
